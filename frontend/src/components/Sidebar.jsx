d22c938751618080fb7fa54ba63e81bb698d7eda
Aditya <pansareaditya281@gmail.com>
2025-07-09 20:55:26 +0530

    logout endpoint added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import assets, { userDummyData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Sidebar = (props) => {
+  const {logout}=useContext(AuthContext);
   const navigate = useNavigate();
   return (
     <div
@@ -28,7 +30,9 @@ const Sidebar = (props) => {
               </p>
 
               <hr className="my-2 border-t border-gray-500" />
-              <p className="cursor-pointer text-sm">Logout</p>
+              <p onClick={()=>{
+                logout();
+              }} className="cursor-pointer text-sm">Logout</p>
             </div>
           </div>
         </div>
