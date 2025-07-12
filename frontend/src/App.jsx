6c13e84919f1c7c06dc843a24597100742e095dc
Aditya <pansareaditya281@gmail.com>
2025-07-12 14:25:37 +0530

    background image added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from "./context/AuthContext";
 const App = () => {
   const { token } = useContext(AuthContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster />
       <Routes>
         <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
